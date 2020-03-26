822561981d4560afd246d9105be26600dfd511ed
aylement <33618420+aylement@users.noreply.github.com>
2020-03-26 15:25:29 +0100

    first part is done

--- Cartes/XmlCartes/index.js
+++ Cartes/XmlCartes/index.js
@@ -1,21 +1,43 @@
-const tableys = document.querySelector("#jsonTable > tbody")
-function loadData () {
-const request = new XMLHttpRequest();
-request.open("GET", "https://arfp.eu/dataset/cards.json");
-request.onload = () => {
-  try {
-    const cards = JSON.parse(request.responseText);
-    putData(cards);
-  }   catch (e){
-        console.warn("error loading the json");
-  } 
+window.addEventListener("DOMContentLoaded", function(event) {
+
+  var tableys = document.getElementById('jsonTable');
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://arfp.eu/dataset/cards.json', true);
+request.onload = function() {
+  if (this.status === 200) {
+      var cards = JSON.parse(this.responseText);
+      for(var i=0; i < cards.length; i++)
+      {
+          tableys.innerHTML += '<td>' + cards[i].id +'</td>' +
+          '<td>' + cards[i].name + '</td>' +
+          '<td>' + cards[i].level + '</td>' +
+          '<td>' + cards[i].description + '</td>' +
+          '<td>' + cards[i].power + '</td>' +
+          '<td>' + cards[i].attack + '</td>' +
+          '<td>' + cards[i].armor + '</td>' +
+          '<td>' + cards[i].damage + '</td>' +
+          '<td>' + cards[i].mitigation + '</td>' +
+          '<td>' + cards[i].played + '</td>' +
+          '<td>' + cards[i].victory + '</td>' +
+          '<td>' + cards[i].defeat + '</td>' +
+          '<td>' + cards[i].draw + '</td>';
+          var maxPpg = getMax(cards, "attack");
+          console.log("Top scorer: " + maxPpg.player + " - " + maxPpg.team);
+         
+      }
+  }
 };
 request.send();
+
+function getMax(arr, prop) {
+  var max;
+  for (var i=0 ; i<arr.length ; i++) {
+      if (!max || parseInt(arr[i][prop]) > parseInt(max[prop]))
+          max = arr[i];
+  }
+  return max;
 }
 
-function putData (cards) {
-      cards.forEach((row) => {
-       console.log(row);
-    });
-}
-document.addEventListener("DOMContentLoaded", () => { loadData(); });
+
+});
